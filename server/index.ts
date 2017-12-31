74cb115438c941b68cd2003829cbde018b9a7da7
bwilm <32461222+bwilm@users.noreply.github.com>
2017-12-30 22:27:55 -0500

    updates

--- server/index.ts
+++ server/index.ts
@@ -2,8 +2,7 @@ import api from './procedures/api';
 import * as express from 'express';
 
 import * as path from 'path';
-import * as bodyParser from 'body-parser';
-import * as cookieParser from 'cookie-parser';
+
 
 
 let routing = require('./middleware/routing.mw.js');
@@ -15,18 +14,16 @@ let dataPath = path.join(__dirname, 'data.json');
 
 let app = express();
 app.use(express.static(clientPath));
-app.use(cookieParser());
-app.use(bodyParser.json());
+
 
 
 app.use('/api', api);
 
 app.get('*', routing.stateRouting);
 
-const prerender = require('prerender-node');
-prerender.set('prerenderToken', process.env.PRERENDER_TOKEN);
-app.use(prerender);
-prerender.set('prerenderServiceUrl', 'http://localhost:1337/');
+
+
+
 
 
 console.log('listening')
